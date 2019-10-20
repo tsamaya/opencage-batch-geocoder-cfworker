81cde3b8c898f7eba75c7197bf764bd1e7f961a5
arnaud ferrand <arferrand@gmail.com>
2019-10-20 08:07:09 +0100

    nice ui

--- index.js
+++ index.js
@@ -5,24 +5,86 @@ addEventListener('fetch', event => {
 import { API_KEY } from './env';
 
 const landing = `
-<h1>OpenCage Geocoder</h1>
-<p>Enter an address:</p>
-<input type="text" id="address" value="Brandenburg Gate"></input>
-<button onclick='geocode()'>Geocode</button>
-<pre id="response"></pre>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>OpenCage batch Geocoder</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
+    />
+    <script
+      defer
+      src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"
+    ></script>
+  </head>
+  <body>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">
+          OpenCage Geocoder
+        </h1>
+        <div class="columns is-mobile">
+          <div class="column">
+            <div class="field">
+              <label class="label">API Key</label>
+              <div class="control">
+                <input
+                  id="key"
+                  class="input"
+                  type="text"
+                  placeholder="API Key"
+                />
+              </div>
+            </div>
 
-<script>
-  async function geocode() {
-    const response = await fetch(window.location.pathname, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ q: document.querySelector("#address").value })
-    });
-    const data = await response.json();
-    console.log(data);
-    document.querySelector("#response").innerHTML = JSON.stringify(data, null, '  ')
-  }
-</script>
+            <div class="field">
+              <label class="label">Address</label>
+              <div class="control">
+                <input
+                  id="address"
+                  class="input"
+                  type="text"
+                  placeholder="Address"
+                  value="Brandenburg Gate"
+                />
+              </div>
+            </div>
+
+            <button class="button is-primary" onclick="geocode()">
+              Geocode
+            </button>
+          </div>
+
+          <div class="column">
+            <pre id="response"></pre>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script>
+      async function geocode() {
+        const response = await fetch(window.location.pathname, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            q: document.querySelector('#address').value,
+            key: document.querySelector('#key').value,
+          }),
+        });
+        const data = await response.json();
+        console.log(data);
+        document.querySelector('#response').innerHTML = JSON.stringify(
+          data,
+          null,
+          '  ',
+        );
+      }
+    </script>
+  </body>
+</html>
 `;
 
 const geocode = async request => {
